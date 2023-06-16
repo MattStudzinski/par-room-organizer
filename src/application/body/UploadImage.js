1eabe09b14d7d6f17fcbce8588cafa101eb622d5
Matthew Studzinski <88520969+MattStudzinski@users.noreply.github.com>
2023-06-15 21:56:38 -0700

    can split array on room name or company number

--- src/application/body/UploadImage.js
+++ src/application/body/UploadImage.js
@@ -32,8 +32,11 @@ const convertImageToText = useCallback(async () => {
     }
   }
 
-  const splitTextToArray = convertedText.split(" ")
+  const splitTextToArray = convertedText.split("001")
   console.log(splitTextToArray)
+
+  const stringArray = splitTextToArray.toString(" ")
+  console.log(stringArray)
   
 
   return (
@@ -64,70 +67,4 @@ const convertImageToText = useCallback(async () => {
 
 
 export default UploadImage;
-
-
-
-
-
-
-    //   const [selectedImage, setSelectedImage] = useState(null);
-    //   const [textResult, setTextResult] = useState("");
-    
-      
-    //   const worker = createWorker();
-    
-    //   const convertImageToText = useCallback(async () => {
-    //     if(!selectedImage) return;
-    //     await worker.load();
-    //     await worker.loadLanguage("eng");
-    //     await worker.initialize("eng");
-    //     const { data } = await worker.recognize(selectedImage);
-    //     setTextResult(data.text);
-    //   }, [worker, selectedImage]);
-    
-    //   useEffect(() => {
-    //     convertImageToText();
-    //   }, [selectedImage, convertImageToText])
-    
-    //   const handleChangeImage = e => {
-    //     if(e.target.files[0]) {
-    //       setSelectedImage(e.target.files[0]);
-    //     } else {
-    //       setSelectedImage(null);
-    //       setTextResult("")
-    //     }
-    //   }
-    
-    //   const convertedArray = textResult.split(" ")
-    //   console.log(convertedArray)
-    
-    
-    //   return (
-    //     <div className="App">
-    //       <h1>ImText</h1>
-    //       <p>Gets words in image!</p>
-    //       <div className="input-wrapper">
-    //         <label htmlFor="upload">Upload Image</label>
-    //         <input type="file" id="upload" accept='image/*' onChange={handleChangeImage} />
-    //       </div>
-    
-    //       <div className="result">
-    //         {selectedImage && (
-    //           <div className="box-image">
-    //             <img src={URL.createObjectURL(selectedImage)} alt="thumb" />
-    //           </div>
-    //         )}
-    //         {textResult && (
-    //           <div className="box-p">
-    //             <p>{textResult}</p>
-                
-    //           </div>
-    //         )}
-    //       </div>
-    //     </div>
-    //   );
-    // }
-    
-    // export default App;
-    
     
